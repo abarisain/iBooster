a73e29ffd5ae6ea4c83d5c11a8e170cdfbb78663
Arnaud Barisain Monrose <abarisain@gmail.com>
2012-11-27 11:45:49 -0500

    dBooster mac

---
+++ Mac/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  dBooster
+//
+//  Created by Arnaud Barisain Monrose on 27/11/12.
+//  Copyright (c) 2012 Arnaud Barisain Monrose. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ Mac/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  dBooster
+//
+//  Created by Arnaud Barisain Monrose on 27/11/12.
+//  Copyright (c) 2012 Arnaud Barisain Monrose. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
